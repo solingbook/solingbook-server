71b87c3e101e9d3ce90b775ccdb9487e7800abe1
siwon <ksw3721@gmail.com>
2026-02-22 11:20:43 +0900

    BooksModule import 추가

--- src/reviews/reviews.module.ts
+++ src/reviews/reviews.module.ts
@@ -4,9 +4,10 @@ import { ReviewsService } from './reviews.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Review } from './review.entity';
 import { TypeOrmReviewRepository } from './reviews.repository';
+import { BooksModule } from 'src/books/books.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Review])],
+  imports: [TypeOrmModule.forFeature([Review]), BooksModule],
   exports: [TypeOrmModule, TypeOrmReviewRepository],
   providers: [ReviewsService, TypeOrmReviewRepository],
   controllers: [ReviewsController],
